0e5a5443f7fc7108e11bdae85df501a9744d206c
nathanGouacheg22017449 <nathan.gouache@etu.univ-amu.fr>
2023-06-02 14:35:02 +0200

    Premier design + fonctionnalité des points

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -10,14 +10,18 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Point2D;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -31,9 +35,88 @@ public class ToileController implements Initializable {
     private static int angleDepart = 90;
     private static int noteMaximale = 20;
 
+    @FXML
+    private Label error;
+
+    @FXML
+    private Circle p1;
+    @FXML
+    private Circle p2;
+    @FXML
+    private Circle p3;
+    @FXML
+    private Circle p4;
+    @FXML
+    private Circle p5;
+    @FXML
+    private Circle p6;
+
+    @FXML
+    TextField t1;
+    @FXML
+    TextField t2;
+    @FXML
+    TextField t3;
+    @FXML
+    TextField t4;
+    @FXML
+    TextField t5;
+    @FXML
+    TextField t6;
+
+    @FXML
+    private void click1() {
+        String texte = t1.getText();
+        p1.setLayoutX(getXRadarChart(Double.valueOf(texte), 1));
+        p1.setLayoutY(getYRadarChart(Double.valueOf(texte), 1));
+        p1.setOpacity(1);
+    }
+
+    @FXML
+    private void click2() {
+        String texte = t2.getText();
+        p2.setLayoutX(getXRadarChart(Double.valueOf(texte), 2));
+        p2.setLayoutY(getYRadarChart(Double.valueOf(texte), 2));
+        p2.setOpacity(1);
+    }
+
+    @FXML
+    private void click3() {
+        String texte = t3.getText();
+        p3.setLayoutX(getXRadarChart(Double.valueOf(texte), 3));
+        p3.setLayoutY(getYRadarChart(Double.valueOf(texte), 3));
+        p3.setOpacity(1);
+    }
+
+    @FXML
+    private void click4() {
+        String texte = t4.getText();
+        p4.setLayoutX(getXRadarChart(Double.valueOf(texte), 4));
+        p4.setLayoutY(getYRadarChart(Double.valueOf(texte), 4));
+        p4.setOpacity(1);
+    }
+
+    @FXML
+    private void click5() {
+        String texte = t5.getText();
+        p5.setLayoutX(getXRadarChart(Double.valueOf(texte), 5));
+        p5.setLayoutY(getYRadarChart(Double.valueOf(texte), 5));
+        p5.setOpacity(1);
+    }
+
+    @FXML
+    private void click6() {
+        String texte = t6.getText();
+        p6.setLayoutX(getXRadarChart(Double.valueOf(texte), 6));
+        p6.setLayoutY(getYRadarChart(Double.valueOf(texte), 6));
+        p6.setOpacity(1);
+    }
+
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 
     int getXRadarChart(double value, int axe ){
@@ -45,5 +128,7 @@ public class ToileController implements Initializable {
         return (int) (rayonCercleExterieur - Math.sin(Math.toRadians(angleDepart - (axe-1)  * angleEnDegre)) * rayonCercleExterieur
                 *  (value / noteMaximale));
     }
+
+
 
 }
